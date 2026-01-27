0ae49752cca8955c396eab1d1b080ab70858e044
caseykuhn05 <caseyksuperstar1@gmail.com>
2026-01-27 14:18:57 -0500

    Update perl_script.pl

--- SNMP_Project1/perl_script.pl
+++ SNMP_Project1/perl_script.pl
@@ -2,49 +2,23 @@
 use strict;
 use warnings;
 
-# Possible log file locations
-my @log_files = (
-    '/var/log/auth.log',
-    '/var/log/secure'
-);
+# check auth logs for sudo sessions
+my @logs = ('/var/log/auth.log', '/var/log/secure');
+my $cnt = 0;
+my $found = 0;
 
-my $sudo_count = 0;
-my $found_log  = 0;
-
-foreach my $log_file (@log_files) {
-
-    # Check if the file exists
-    if (-e $log_file) {
-        $found_log = 1;
-
-        # Try to open the file
-        if (open(my $fh, '<', $log_file)) {
-
-            while (my $line = <$fh>) {
-                if ($line =~ /sudo:.*session opened for user/i) {
-                    $sudo_count++;
-                }
+foreach my $file (@logs) {
+    if (-e $file) {
+        $found = 1;
+        if (open(my $fh, '<', $file)) {
+            while (<$fh>) {
+                $cnt++ if /sudo:.*session opened/i;
             }
-
             close($fh);
-            last;   # Stop after successfully reading one log file
-
-        } else {
-            # Permission or access error
-            print "Cannot read $log_file: $!\n";
-            print "Try running as root or ensure the user has permission to read log files.\n";
-            exit 1;
+            last; # found active log
         }
     }
 }
 
-# If no log file existed at all
-if (!$found_log) {
-    print "No system log file found (checked /var/log/auth.log and /var/log/secure).\n";
-    exit 1;
-}
-
-# Output the count
-print $sudo_count;
-
-
+# output count without newline for prompt continuity
+printf "%d", $cnt;
